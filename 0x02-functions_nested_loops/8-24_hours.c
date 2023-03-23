c1a236df6ae9803585dfa0bb72e95b1525fb5b34
youssef <youssefelmelegy999@gmail.com>
2023-03-22 19:25:30 -0700

    a

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,7 +12,7 @@ void jack_bauer(void)
 	{
 		for (min = 0; min <= 59; min++)
 		{
-			_putchar((hr \ 10) + 48);
+			_putchar((hr / 10) + 48);
 			_putchar((hr % 10) + 48);
 			_putchar(':');
 			_putchar((min / 10) + 48);
