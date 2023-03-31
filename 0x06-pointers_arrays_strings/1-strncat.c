44ec09efc43b1efb4777d9dd989f51fb18aaec73
youssef <youssefelmelegy999@gmail.com>
2023-03-30 17:16:57 -0700

    a

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -24,31 +24,3 @@ char *_strncat(char *dest, char *src, int n)
 	dest[i + j] = '\0';
 	return (dest);
 }
-2-strncpy.c
-
-#include "main.h"
-
-/**
- *_strncpy - function that copies a string
- *
- * @src: The source of strings
- * @dest: The destination of the string
- * @n: The length of int
- *
- * Return: pointer to the resulting string dest
- */
-char *_strncpy(char *dest, char *src, int n)
-{
-int i;
-
-	for (i = 0; i < n && *(src + i); i++)
-	{
-		*(dest + i) = *(src + i);
-	}
-	for (; i < n; i++)
-	{
-	*(dest + i) = '\0';
-	}
-	return (dest);
-
-}
