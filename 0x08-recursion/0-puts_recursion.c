ea7a184fb7cbbb5bea2340534fae1c57358f97f4
youssef <youssefelmelegy999@gmail.com>
2023-09-18 00:43:21 -0700

    1st

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if  (*s)
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
 	{
 		_putchar(*s);
-		_put_recursion(s + 1);
+		_puts_recursion(s + 1);
 	}
-
-	else
-		_putchar('\n');
 }
