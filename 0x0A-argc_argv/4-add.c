9ce675c48b9a1631c725a615d5e66afcaaf3f09a
youssef elmelegy <youssefelmelegy999@gmail.com>
2023-10-28 10:40:25 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,7 +5,8 @@
  * main - program that adds positive numbers.
  * @argc: int
  * @argv: list
- * Return: 0
+ *
+ *  Return: Always 0
  */
 
 int main(int argc, char *argv[])
@@ -16,7 +17,6 @@ int main(int argc, char *argv[])
 	while (--argc)
 	{
 		for (c = argv[argc]; *c; c++)
-		{
 			if (*c < '0' || *c > '9')
 				return (printf("Error\n"), 1);
 		sum += atoi(argv[argc]);
