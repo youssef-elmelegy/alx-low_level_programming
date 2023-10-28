0dee550d7a1110d9b7e8d5b64921eec2642748cc
youssef elmelegy <youssefelmelegy999@gmail.com>
2023-10-28 07:24:43 -0700

    a

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the number of arguments ,followed by a new line
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char const **argv __attribute__((unused)))
+{
+	printf("%d\n", arg - 1);
+	return (0);
+}
