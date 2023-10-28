c7ed1a5c93ebce3eb780fc367bef1a7cfb1bdbd8
youssef elmelegy <youssefelmelegy999@gmail.com>
2023-10-28 07:14:12 -0700

    a

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,11 +2,12 @@
 
 /**
  * main - prints a program name, followed by a new line
+ * @argc: int
  * @argv: list
  * Return: 0
 */
 
-int main(char const *argv[])
+int main(int argc, char const **argv)
 {
 	printf("%s\n", argv[0]);
 	return (0);
