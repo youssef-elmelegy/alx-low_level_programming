02a27eeab585bd7fae44b23f7bc7814f4e152e94
youssef elmelegy <youssefelmelegy999@gmail.com>
2023-10-28 08:24:30 -0700

    a

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all arguments it received.
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	for (int i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i];
+	}
+	return (0);
+}
