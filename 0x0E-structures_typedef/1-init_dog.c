193e691406f50337eff9d461c975807084a0f528
youssef elmelegy <youssefelmelegy999@gmail.com>
2023-11-06 15:25:44 -0800

    var

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,8 +14,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-		d->name = Pobby;
-		d->age = 3.5;
-		d->owner = Bob;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
