0f6f5062548b16169590df5449d41fcc057543db
youssef elmelegy <youssefelmelegy999@gmail.com>
2023-11-06 15:06:00 -0800

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG.H
+#define DOG.H
+
+/**
+ * struct dog - dog struct
+ * @name: char*
+ * @age: float
+ * @owner: char*
+ *
+ * Descriptiona: long dog struct 
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
