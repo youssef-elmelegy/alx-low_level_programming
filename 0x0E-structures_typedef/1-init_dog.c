245a7390cc7a27bda3ce8fcd1b33cb2173d50d2b
youssef elmelegy <youssefelmelegy999@gmail.com>
2023-11-06 15:22:14 -0800

    var

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize variables
+ * *d: struct of dog
+ * *name: dog's name
+ * *owner: dog's owner
+ *
+ * Return: void.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = Pobby;
+		d->age = 3.5;
+		d->owner = Bob;
+	}
+}
