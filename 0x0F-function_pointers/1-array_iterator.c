7d44bc79aa6d3546ce1a14d360a794fc62fdb1fc
youssef elmelegy <youssefelmelegy999@gmail.com>
2023-11-08 15:23:03 -0800

    a

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,9 +9,12 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	unsigned int i;
+
 	if (array == NULL || action == NULL)
 		return;
-	for (int i = 0; i < size; i++)
+
+	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
 	}
