7d99c6a2989a5560b97727e5525d927f3c1ecf06
youssef-elmelegy <115453231+youssef-elmelegy@users.noreply.github.com>
2023-12-11 16:38:13 +0200

    Update _fork.c

--- yshell/_fork.c
+++ yshell/_fork.c
@@ -1,24 +1,20 @@
 #include "main.h"
 
-
 /**
  * _fork - create a child to execute command
- * @new_argv: string of command and arguments.
+ * @new_argv: string of command and arguments
  * Return: 0 for parent
 */
 
 int _fork(char **new_argv)
 {
-	pid_t child = 0;
-	int status = 0;
+	pid_t child = fork();
 
-	child = fork();
 	if (child == -1)
 	{
-		perror("Fork failed");
+		perror("fork failed");
 		return (-1);
 	}
-
 	if (child == 0)
 	{
 		execmd(new_argv);
@@ -26,6 +22,8 @@ int _fork(char **new_argv)
 	}
 	else
 	{
+		int status = 0;
+
 		wait(&status);
 	}
 	return (0);
