d10ec01f86f5f86d08c2457c8896e340274ceedd
youssef-elmelegy <115453231+youssef-elmelegy@users.noreply.github.com>
2023-12-11 16:39:32 +0200

    Update main.c

--- yshell/main.c
+++ yshell/main.c
@@ -47,44 +47,46 @@ int main(int ac, char **argv)
 }
 
 
-
 char *get_loc(char *command)
 {
-	char *path = NULL, *path_copy = NULL, *path_token = NULL, *file_path = NULL;
-	int command_length = 0, directory_length = 0;
+	char *path_copy = NULL, *path_token = NULL, *file_path = NULL;
+	int command_length = 0;
 	struct stat buffer;
+	char *path = getenv("PATH");
 
-	path = getenv("PATH");
-	if (path)
+	if (!path)
 	{
-		path_copy = str_duplicate(path);
-		command_length = str_length(command);
-		path_token = strtok(path_copy, ":");
-		while (path_token != NULL)
-		{
-			directory_length = str_length(path_token);
-			file_path = malloc(command_length + directory_length + 1);
-			_strcpy(file_path, path_token);
-			_strcat(file_path, "/");
-			_strcat(file_path, command);
-			_strcat(file_path, "\0");
-			if (stat(file_path, &buffer) == 0)
-			{
-				free(path_copy);
-				return (file_path);
-			}
-			else
-			{
-				free(file_path);
-				path_token = strtok(NULL, ":");
-			}
-		}
-		free(path_copy);
-		if (stat(command, &buffer) == 0)
-		{
-			return (command);
-		}
 		return (NULL);
+	}
+	path_copy = str_duplicate(path);
+	command_length = str_length(command);
+	path_token = strtok(path_copy, ":");
+	file_path = NULL;
+	while (path_token)
+	{
+		int directory_length = str_length(path_token);
+
+		file_path = malloc(command_length + directory_length + 2);
+		if (!file_path)
+		{
+			free(path_copy);
+			return (NULL);
+		}
+		strncpy(file_path, path_token, directory_length);
+		file_path[directory_length] = '/';
+		strncpy(file_path + directory_length + 1, command, command_length + 1);
+		if (stat(file_path, &buffer) == 0)
+		{
+			free(path_copy);
+			return (file_path);
+		}
+		free(file_path);
+		path_token = strtok(NULL, ":");
+	}
+	free(path_copy);
+	if (stat(command, &buffer) == 0)
+	{
+		return (str_duplicate(command));
 	}
 	return (NULL);
 }
