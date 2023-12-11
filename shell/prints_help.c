346f6eba32df662a56cd16c9680ac27c0e406b30
youssef-elmelegy <115453231+youssef-elmelegy@users.noreply.github.com>
2023-12-11 05:21:36 +0200

    Update prints_help.c

--- shell/prints_help.c
+++ shell/prints_help.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print - writes a array of chars in the standar output
+ * _print - writes a array of chars in the standar output..
  * @string: pointer to the array of chars
  * Return: the number of bytes writed or .
  * On error, -1 is returned, and errno is set appropriately.
